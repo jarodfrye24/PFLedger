8161ba748b63792467e0302108f967adf2c92121
jarodfrye24 <jarodfrye24@gmail.com>
2025-07-19 06:36:28 -0400

    PF1 V11 Update

--- scripts/ledger.js
+++ scripts/ledger.js
@@ -257,7 +257,7 @@ function addLedgerButtons(sheet, jq, data)
     deleteLedgerButton.classList.add("pfledger-button");
     deleteLedgerButton.textContent = "Delete Ledger";
     deleteLedgerButton.addEventListener("click", event => {
-        LedgerData.currentLedgers.remove;
+        LedgerData.remove(actor)
     });
     currencyTab.append(deleteLedgerButton);
 }
